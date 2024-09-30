03801ba2c38bc29d46ed9dd66701ced05a032625
Leonid Kozhenkin <lkozhenkin@gmail.com>
2024-09-30 10:10:16 +0300

    add playground

---
+++ child-hub/src/main/java/org/ipr/childhub/FluxPlayground.java
@@ -0,0 +1,17 @@
+/*
+package org.ipr.childhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class FluxPlayground {
+
+    public static void main(String[] args) {
+        Mono.just(2).doOnEach(System.out::println).subscribe();
+    }
+
+}
+*/
