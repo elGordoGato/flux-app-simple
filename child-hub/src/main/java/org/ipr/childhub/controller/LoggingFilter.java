4362fcaff1d5494edfceb071f8a527b1f4ce1a8f
Leonid Kozhenkin <lkozhenkin@gmail.com>
2024-09-30 18:34:34 +0300

    add elk

--- child-hub/src/main/java/org/ipr/childhub/controller/LoggingFilter.java
+++ child-hub/src/main/java/org/ipr/childhub/controller/LoggingFilter.java
@@ -1,6 +1,7 @@
 package org.ipr.childhub.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -8,14 +9,14 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 import reactor.util.annotation.NonNull;
-import reactor.util.annotation.NonNullApi;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class LoggingFilter implements WebFilter {
+    private static final String REQUEST_ID = "requestId";
 
     @Override
     public @NonNull Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
@@ -24,6 +25,8 @@ public class LoggingFilter implements WebFilter {
         }
         long startTime = Instant.now().toEpochMilli();
         ServerHttpRequest httpRequest = exchange.getRequest();
+        String requestId = UUID.randomUUID().toString();
+        MDC.put(REQUEST_ID, requestId);
         log.info("Incoming request: method={}, path={}, params={}",
                 httpRequest.getMethod(),
                 httpRequest.getURI().getPath(),
